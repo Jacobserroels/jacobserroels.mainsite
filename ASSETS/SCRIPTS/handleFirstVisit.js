a234493e47fbeb49766aad9ca0bfbc017735a970
Jacob Serroels <jacob@jacobserroels.com>
2022-04-21 16:57:17 -0700

    Functionality Scripts

--- ASSETS/SCRIPTS/handleFirstVisit.js
+++ ASSETS/SCRIPTS/handleFirstVisit.js
@@ -2,13 +2,13 @@
 
 
 
-//var firstVisitQuerey = localStorage.setItem('hasVisted',0)
-//
-//if (firstVisitQuerey == null){
-//	firstVisitQuerey = localStorage.setItem('hasVisted',1)
-//	window.location = "http://test.jacobserroels.com";
-//} else if (firstVisitQuerey == 0) {
-//	firstVisitQuerey = localStorage.setItem('hasVisted',1)
-//	window.location = "http://test.jacobserroels.com";
-//	}
-//else{}
+localStorage.setItem('hasVisted',0)
+var firstVisitQuerey = localStorage.getItem('hasVisted')
+
+if (firstVisitQuerey == null){
+	firstVisitQuerey = localStorage.setItem('hasVisted',1)
+	window.location = "http://test.jacobserroels.com";
+} else if (firstVisitQuerey == 0) {
+	firstVisitQuerey = localStorage.setItem('hasVisted',1)
+	window.location = "http://test.jacobserroels.com";
+	}
---
+++ ASSETS/SCRIPTS/handleGridOnOff.js
@@ -0,0 +1,31 @@
+// JavaScript Document
+
+var gridOnOff = document.getElementById("gridButton");
+var head = document.getElementsByTagName('head')[0];
+
+localStorage.setItem('gridSetting',0)
+var gridQuerey = localStorage.getItem('gridSetting')
+
+
+gridOnOff.addEventListener("click", gridHandler);
+
+if(gridQuerey == 1){
+		var cssGrid = document.createElement('link');
+		cssGrid.rel = "stylesheet";
+		cssGrid.href = 'STYLESHEETS/grid-css.css';
+		head.appendChild(cssGrid);
+	}
+
+function gridHandler(){
+	if(gridQuerey == null){
+		console.log('loading main page');
+		gridQuerey = localStorage.setItem('gridSetting', 1);
+		window.location = "http://test.jacobserroels.com";
+	}
+	else if(gridQuerey == 0){
+		console.log('loading main page');
+		gridQuerey = localStorage.setItem('gridSetting', 1);
+		window.location = "http://test.jacobserroels.com";
+	}
+	
+}
