7bf6d0ecdfc4a6b30a112802ed5bd48ee575c975
Jacob Serroels <jacob@jacobserroels.com>
2022-04-22 09:28:35 -0700

    Grid Script 1.55

--- ASSETS/SCRIPTS/handleGridOnOff.js
+++ ASSETS/SCRIPTS/handleGridOnOff.js
@@ -2,28 +2,30 @@
 
 var gridOnOff = document.getElementById("gridButton");
 var body = document.getElementsByTagName('body')[0];
+var getPage = window.location.href;
 
 var gridQuerey = sessionStorage.getItem('gridSetting');
 sessionStorage.setItem('gridSetting', 1);
-
-
-gridOnOff.addEventListener("click", gridHandler);
 
 if(gridQuerey == 1){
 	body.style.backgroundImage = "linear-gradient(to right, var(--darkColor) 1px, transparent 1px), linear-gradient(to bottom, var(--darkColor) 1px, transparent 1px)"
 	body.style.backgroundSize = "40px 40px";
 	}
 
+if (getPage == "test.jacobserroels.com/applied-design"); {
+	gridOnOff.addEventListener("click", gridHandler);
+}
+
 function gridHandler(){
 	if(gridQuerey == null){
 		console.log('loading main page');
 		sessionStorage.setItem('gridSetting', 1);
-		window.location = "http://test.jacobserroels.com";
+		window.location.assign = "http://test.jacobserroels.com";
 	}
 	else if(gridQuerey == 0){
 		console.log('loading main page');
 		sessionStorage.setItem('gridSetting', 1);
-		window.location = "http://test.jacobserroels.com";
+		window.location.assign = "http://test.jacobserroels.com";
 	}
 	
 }
