d305092a81b2d47468d41ddca59f21fcb3e49ec9
Jacob Serroels <jacob@jacobserroels.com>
2022-04-22 09:15:24 -0700

    grid script testing

--- ASSETS/SCRIPTS/handleGridOnOff.js
+++ ASSETS/SCRIPTS/handleGridOnOff.js
@@ -3,8 +3,7 @@
 var gridOnOff = document.getElementById("gridButton");
 var body = document.getElementsByTagName('body')[0];
 
-var gridQuerey = localStorage.getItem('gridSetting');
-var firstVisit = localStorage.getItem('hasVisted');
+var gridQuerey = sessionStorage.getItem('gridSetting')
 
 
 gridOnOff.addEventListener("click", gridHandler);
@@ -13,22 +12,16 @@ if(gridQuerey == 1){
 	body.style.backgroundImage = "linear-gradient(to right, var(--darkColor) 1px, transparent 1px), linear-gradient(to bottom, var(--darkColor) 1px, transparent 1px)"
 	body.style.backgroundSize = "40px 40px";
 	}
-if(firstVisit == null){
-	localStorage.setItem('gridSetting', 0);
-}
-else if(firstVisit == 0){
-	localStorage.setItem('gridSetting', 0);
-}
 
 function gridHandler(){
 	if(gridQuerey == null){
 		console.log('loading main page');
-		localStorage.setItem('gridSetting', 1);
+		sessionStorage.setItem('gridSetting', 1);
 		window.location = "http://test.jacobserroels.com";
 	}
 	else if(gridQuerey == 0){
 		console.log('loading main page');
-		localStorage.setItem('gridSetting', 1);
+		sessionStorage.setItem('gridSetting', 1);
 		window.location = "http://test.jacobserroels.com";
 	}
 	
