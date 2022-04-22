0d4b56932869ffd2e3408b0a30f968cdc282be68
Jacob Serroels <jacob@jacobserroels.com>
2022-04-22 08:58:08 -0700

    help

--- ASSETS/SCRIPTS/handleFirstVisit.js
+++ ASSETS/SCRIPTS/handleFirstVisit.js
@@ -1,17 +1,17 @@
-// Script for handling first time visitors
+ //Script for handling first time visitors
 
 
 
-//localStorage.setItem('hasVisted',0);
-//var firstVisitQuerey = localStorage.getItem('hasVisted');
-//
-//if (firstVisitQuerey == null){
-//	localStorage.setItem('hasVisted',1);
-//	window.location = "http://test.jacobserroels.com";
-//} else if (firstVisitQuerey == 0) {
-//	localStorage.setItem('hasVisted',1);
-//	window.location = "http://test.jacobserroels.com";
-//	}
-//else if (firstVisitQuerey == 1){
-//	localStorage.setItem('hasVisted',1);
-//}
+localStorage.setItem('hasVisted',0);
+var firstVisitQuerey = localStorage.getItem('hasVisted');
+
+if (firstVisitQuerey == null){
+	localStorage.setItem('hasVisted',1);
+	window.location = "http://test.jacobserroels.com";
+} else if (firstVisitQuerey == 0) {
+	localStorage.setItem('hasVisted',1);
+	window.location = "http://test.jacobserroels.com";
+	}
+else if (firstVisitQuerey == 1){
+	localStorage.setItem('hasVisted',1);
+}
