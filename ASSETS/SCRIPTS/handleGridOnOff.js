7513a3666f1ed5f0598f6079e1081c645afd230d
Jacob Serroels <jacob@jacobserroels.com>
2022-04-22 09:05:51 -0700

    grid script 1.75

--- ASSETS/SCRIPTS/handleGridOnOff.js
+++ ASSETS/SCRIPTS/handleGridOnOff.js
@@ -3,8 +3,7 @@
 var gridOnOff = document.getElementById("gridButton");
 var body = document.getElementsByTagName('body')[0];
 
-localStorage.setItem('gridSetting',0)
-var gridQuerey = localStorage.getItem('gridSetting')
+var gridQuerey = sessionStorage.getItem('gridSetting')
 
 
 gridOnOff.addEventListener("click", gridHandler);
@@ -17,12 +16,12 @@ if(gridQuerey == 1){
 function gridHandler(){
 	if(gridQuerey == null){
 		console.log('loading main page');
-		localStorage.setItem('gridSetting', 1);
+		sessionStorage.setItem('gridSetting', 1);
 		window.location = "http://test.jacobserroels.com";
 	}
 	else if(gridQuerey == 0){
 		console.log('loading main page');
-		localStorage.setItem('gridSetting', 1);
+		sessionStorage.setItem('gridSetting', 1);
 		window.location = "http://test.jacobserroels.com";
 	}
 	
