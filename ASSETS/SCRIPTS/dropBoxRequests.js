bb5428c95bd1fbc72f37867a20ff5a228087c4fc
Jacobserroels <jacob@jacobserroels.com>
2022-05-02 12:55:03 -0700

    trying to get a key

--- ASSETS/SCRIPTS/dropBoxRequests.js
+++ ASSETS/SCRIPTS/dropBoxRequests.js
@@ -1,20 +1,5 @@
-var data = JSON.stringify({
-    "oauth1_token": "qievr8hamyg6ndck",
-    "oauth1_token_secret": "qomoftv0472git7"
-  });
-  
-  var xhr = new XMLHttpRequest();
-  xhr.withCredentials = true;
-  
-  xhr.addEventListener("readystatechange", function() {
-    if(this.readyState === 4) {
-      console.log(this.responseText);
-    }
-  });
-  
-  xhr.open("POST", "https://api.dropboxapi.com/2/auth/token/from_oauth1");
-  xhr.setRequestHeader("Authorization", "Basic ODNtengzbHBxaXJseGE0Onk5YWpwZ2V2a3dwMzBudQ==");
-  xhr.setRequestHeader("Content-Type", "application/json");
-  xhr.setRequestHeader("Access-Control-Allow-Origin", "jacobserroels.com");
-  
-  xhr.send(data);
+var auth = new XMLHttpRequest
+auth.open('GET', "https://www.dropbox.com/oauth2/authorize?client_id=<83mzx3lpqirlxa4>&response_type=code", false)
+auth.send()
+
+console.log(auth)
